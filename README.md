e36a7c3850007795d9025f976f958e1fad3a7b38
Pablo <pablotilotta@vb-mundo.com>
2018-06-05 21:59:04 -0300

    Primer Commit

---
+++ README.md
@@ -0,0 +1,9 @@
+## Aplicación del Clima - Curso Node
+
+Recuerden ejecutar ```npm install``` para las librerías
+
+### Ejemplo:
+```
+nore app -d "Buenos Aires, Argentina"
+
+```
---
+++ app.js
@@ -0,0 +1,27 @@
+const lugar = require('./lugar/lugar');
+const clima = require('./clima/clima');
+
+const argv = require('yargs').options({
+    direccion: {
+        alias: 'd',
+        desc: 'Dirección de la ciudad para obtener el clima',
+        demand: true
+    }
+}).argv;
+
+let getInfo = async() => {
+    try {
+        let coors = await lugar.getLugarLatLng(argv.direccion);
+        let temp = await clima.getClima(coors.lat, coors.lng);
+        return `\n\nLas coordenadas de ${ coors.direccion } son :\n\n
+       Latitud >${ coors.lat } - Longitud > ${ coors.lng } \n\nY la temperatura actual es de ${temp}`
+
+    } catch (e) {
+        return `No se pudo determinar la información de ${ argv.direccion}`;
+    }
+
+}
+
+getInfo(argv.direccion)
+    .then(resp => console.log(resp))
+    .catch(e => console.log(e));
